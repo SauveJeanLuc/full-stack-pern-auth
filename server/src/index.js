8747fde53b32dd4c182b23dce2ab038d17af22e9
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-06-14 18:29:47 +0200

    starter file containing routes

---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const { PORT } = require('./constants')
+const app = express()
+
+const authRoutes = require('./routes/auth')
+
+app.use('/api', authRoutes)
+
+const startApp = () => {
+    try{
+        app.listen(PORT, () => {
+            console.log(`The app is running at http://localhost:${PORT}/`)
+        })
+    } catch (error) {
+        console.log (`Error: ${error.message}`)
+    }
+}
+ 
+startApp()

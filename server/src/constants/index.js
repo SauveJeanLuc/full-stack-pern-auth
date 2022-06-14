c46f3a771ef1ae3da45db7d5dac9d1ed2712e37b
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-06-14 20:27:16 +0200

    added db config to constants

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -6,4 +6,9 @@ module.exports = {
     SERVER_URL: process.env.SERVER_URL,
     CLIENT_URL: process.env.CLIENT_URL,
     SECRET: process.env.SECRET,
+    USER: process.env.DATABASE_USER,
+    HOST: process.env.HOST,
+    DATABASE: process.env.DATABASE,
+    PASSWORD: process.env.PASSWORD,
+    DATABASE_PORT: process.env.DATABASE_PORT,
 }

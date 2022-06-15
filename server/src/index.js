667a86ae66936cfe40c96e828383e34a34c98d55
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-06-15 10:03:32 +0200

    express.json

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const { PORT } = require('./constants')
 const app = express()
 
+app.use(express.json())
+
 const authRoutes = require('./routes/auth')
+
+
 
 app.use('/api', authRoutes)
 

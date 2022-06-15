507ca6e45ab3bcc110330648a87f886010f3734b
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-06-15 11:52:36 +0200

    login validation

--- server/src/validators/auth.js
+++ server/src/validators/auth.js
@@ -1,5 +1,6 @@
 const { check } = require('express-validator')
 const db = require('../db')
+const {compare} = require('bcryptjs')
 
 const password = check('password').isLength({ min: 6, max: 15 }).withMessage('Password must be between 6 and 15 characters.');
 
@@ -15,6 +16,25 @@ const emailExists = check('email').custom(
     }
 )
 
+const loginFieldsCheck = check('email').custom(
+    async (value, {req}) => {
+
+        const user = await db.query('SELECT * from users where email = $1', [value])
+        
+        if(!user.rows.length){
+            throw new Error('Email or Password is Invalid');
+        }
+        
+        const validPassword = await compare(req.body.password, user.rows[0].password)
+
+        if(!validPassword)
+            throw new Error('Email or Password is Invalid');
+
+        req.user = user.rows[0];
+    }
+)
+
 module.exports = {
     registerValidation : [email, password, emailExists],
+    loginValidation: [loginFieldsCheck],
 }

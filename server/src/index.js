9d260e63003a07c3f8a593188e403b8c6c320229
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-06-16 14:24:40 +0200

    init passport

--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const { PORT } = require('./constants')
 const app = express()
 const cookieParser = require('cookie-parser')
+const passport = require('passport')
+const authRoutes = require('./routes/auth')
+
+require('./middlewares/passport-middleware')
 
 app.use(express.json())
 app.use(cookieParser())
-
-const authRoutes = require('./routes/auth')
-
+app.use(passport.initialize())
 
 
 app.use('/api', authRoutes)

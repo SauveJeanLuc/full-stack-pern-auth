a45e9c9e4664222ac06696697e3badabd1988e9b
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-06-16 14:26:56 +0200

    define passport middleware

---
+++ server/src/middlewares/passport-middleware.js
@@ -0,0 +1,38 @@
+const {SECRET} = require('../constants')
+const db = require('../db')
+const passport = require('passport')
+const { Strategy } = require('passport-jwt')
+
+const cookieExtractor = function (req) {
+    let token = null;
+
+    if( req & req.cookies ) 
+        token = req.cookies('token')
+    return token;
+}
+
+const opts = {
+    secretOrKey: SECRET,
+    jwtFromRequest: cookieExtractor,
+}
+
+
+
+passport.use(
+    new Strategy(opts, async ({id}, done) => {
+        try{
+            const {rows} = await db.query('SELECT id, email FROM users where id = $1', [id])
+
+            if(!rows.length) {
+                throw new Error('401 not authorized')
+            }
+
+            let user = { id: rows[0].id, email: rows[o].email}
+
+            return await done(null, user)
+        } catch (error) {
+            console.log(error.message)
+            done(null, false)
+        }
+    })
+)

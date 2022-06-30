ba210c577c4257d4abf4b05dc32fa5ce4801f09e
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-06-30 22:16:51 +0200

    add logout route + protected

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,15 +1,17 @@
 const {Router} = require('express');
-const { getUsers, register, login, protected } = require('../controllers/auth');
+const { getUsers, register, login, protected, logout } = require('../controllers/auth');
 const { validatorMiddleware } = require('../middlewares/validations-middleware');
 const router = Router();
 const {userAuth} = require('../middlewares/auth-middleware')
 
 const {registerValidation, loginValidation} = require('../validators/auth');
 
+
 router.get('/users', getUsers)
-router.get('/protected',userAuth ,protected)
+router.get('/protected', userAuth ,protected)
 router.post('/register', registerValidation, validatorMiddleware, register)
 router.post('/login', loginValidation, validatorMiddleware, login)
+router.get('/logout', userAuth, logout)
 
 
 module.exports = router;
